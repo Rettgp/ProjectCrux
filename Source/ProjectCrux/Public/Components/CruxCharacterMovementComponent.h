c1d496f2c5953160e7228690761e4398b3a29c57
Garrett <garephelps@live.com>
2020-02-26 12:28:29 -0600

    test

---
+++ Source/ProjectCrux/Public/Components/CruxCharacterMovementComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "CruxCharacterMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTCRUX_API UCruxCharacterMovementComponent : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/ProjectCrux/Public/CruxStatusEffect.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class PROJECTCRUX_API CruxStatusEffect
+{
+public:
+	CruxStatusEffect();
+	~CruxStatusEffect();
+};
